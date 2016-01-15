f90a647d7385cdb383d5dbeef30534015c2e99e1
oventurapiv <oventura@pivotal.io>
2016-01-15 16:13:49 +0000

    Changes on the code

--- src/main/scala/simple/simple.scala
+++ src/main/scala/simple/simple.scala
@@ -14,18 +14,31 @@ object simple {
     val user = sc.getConf.get(ConfigHelper.USER1)
     val host =sc.getConf.get(ConfigHelper.DB1_HOST)
     val port =sc.getConf.get(ConfigHelper.DB1_PORT)
-    //val host ="192.168.177.145"
-    //val port = "5432"
+    val user2 = sc.getConf.get(ConfigHelper.USER2)
+    val host2 =sc.getConf.get(ConfigHelper.DB2_HOST)
+    val port2 =sc.getConf.get(ConfigHelper.DB2_PORT)
+    val password2 =sc.getConf.get(ConfigHelper.PASSWORD2)
+
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     val url = "jdbc:postgresql://" + host + ":" + port + "/gpadmin"
+    val url2 = "jdbc:teradata://" + host2 + "/TMODE=ANSI,DATABASE=dbc,USER=" + user2 + ",PASSWORD=" + password2
     val t1 = sqlContext.read.format("jdbc").options(
       Map(
         "url" -> url,
         "dbtable" -> "test_piani",
         "driver" -> "org.postgresql.Driver",
         "user" -> user)).load()
+    val t2 = sqlContext.read.format("jdbc").options(
+      Map(
+        "url" -> "jdbc:teradata://192.168.0.16/TMODE=ANSI,DATABASE=dbc,USER=dbc,PASSWORD=dbc",
+        "driver" -> "com.teradata.jdbc.TeraDriver",
+        "dbtable" -> "diskspace",
+        "user" -> user2)).load()
+
     val struct1= t1.rdd
     struct1.saveAsTextFile("/Users/oventura/kk.txt")
+    val struct2= t2.rdd
+    struct2.saveAsTextFile("/Users/oventura/kk2.txt")
   }
 }
 
